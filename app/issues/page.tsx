0109f4fb2dbc41bec111eb8e63f5e39ef52a8498
rikoai00010 <rikoai00010@gmail.com>
2023-10-14 07:43:22 +0200

    Setup RadixUI

---
+++ app/issues/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes'
+
+const IssuesPage = () => {
+  return (
+    <div>
+      <Button>NEW ISSUE</Button>
+    </div>
+  )
+}
+
+export default IssuesPage
